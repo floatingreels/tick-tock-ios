42f63f558e55a7d6feb40941e709deb1d804f2b5
David Gunzburg <build@paybonsai.com>
2025-05-26 20:46:28 +0200

    add some translations

--- TickTock/TickTock/Resources/Strings/Translation.swift
+++ TickTock/TickTock/Resources/Strings/Translation.swift
@@ -9,30 +9,37 @@ import SwiftUICore
 import Foundation
 
 enum Translation {
+    
 }
 
 extension Translation {
-    
-    
-    
+        
     enum General: String.LocalizationValue {
+        case buttonNext = "general_button_next"
         case labelEmail = "general_label_email"
         case labelPassword = "general_label_password"
         case labelPasswordConfirm = "general_label_password_confirm"
         case labelFirstName = "general_label_name_first"
         case labelLastName = "general_label_name_last"
+        case passwordRequirements = "general_password_requirements_message"
+        case emailValidation = "general_email_validation_message"
+        case passwordMismatch = "general_password_mismatch"
         
         var val: String {
+            
             let res = LocalizedStringResource(self.rawValue, table: "Localizable")
             return String(localized: res)
         }
     }
     
     enum Startup: String.LocalizationValue {
+        case homeNavTitle = "home_navigation_title"
         case loginNavTitle = "login_navigation_title"
         case registerNavTitle = "register_navigation_title"
         case buttonLogin = "welcome_button_login"
         case buttonRegister = "welcome_button_register"
+        case registerFormDescription = "register_form_description"
+        case loginFormDescription = "login_form_description"
         
         var val: String {
             let res = LocalizedStringResource(self.rawValue, table: "Localizable")
