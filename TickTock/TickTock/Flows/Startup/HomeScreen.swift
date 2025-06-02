8f29915dd16821f2ae3d6a11d29d9de10bd65c1c
David Gunzburg <build@paybonsai.com>
2025-06-02 11:49:10 +0200

    create some buttons to start flows on home screen

--- TickTock/TickTock/Flows/Startup/HomeScreen.swift
+++ TickTock/TickTock/Flows/Startup/HomeScreen.swift
@@ -12,7 +12,54 @@ struct HomeScreen: View {
     @EnvironmentObject private var coordinator: Coordinator
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: Spacing.interItem * 2) {
+            if let first =  TickTockDefaults.shared.firstName,
+               let last = TickTockDefaults.shared.lastName {
+                Text("Hello, \(first) \(last)!")
+            } else {
+                Text(isPreview ? "Hello, Joe Tester!" : "Something went wrong")
+            }
+            VStack(spacing: Spacing.interItem) {
+                addClientButton
+                newProjectButton
+                startSessionButton
+            }
+        }
+        .navigationTitle("Home")
+    }
+    
+    private func addClient() {
+        coordinator.push(.addClient)
+    }
+    
+    private func newProject() {
+    }
+    
+    private func startSession() {
+    }
+}
+
+extension HomeScreen {
+    
+    var addClientButton: some View {
+        Button(action: addClient) {
+            Text(Translation.Startup.buttonAddClient.val)
+        }
+        .accentColor(.labelLinks)
+    }
+    
+    var newProjectButton: some View {
+        Button(action: newProject) {
+            Text(Translation.Startup.buttonNewProject.val)
+        }
+        .accentColor(.labelLinks)
+    }
+    
+    var startSessionButton: some View {
+        Button(action: startSession) {
+            Text(Translation.Startup.buttonStartSession.val)
+        }
+        .accentColor(.labelLinks)
     }
 }
 
