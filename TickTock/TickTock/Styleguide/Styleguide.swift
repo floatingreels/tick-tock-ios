7dc4efb26cefb02845d5a21568f159b85ae92ac1
David Gunzburg <build@paybonsai.com>
2025-06-05 10:23:52 +0200

    update some styling constants

--- TickTock/TickTock/Styleguide/Styleguide.swift
+++ TickTock/TickTock/Styleguide/Styleguide.swift
@@ -15,15 +15,14 @@ final class Spacing {
 
 final class Size {
     static let cornerRadius: CGFloat = 12
-    static let detailHeaderLogo: CGFloat = 64
     static let listItemInnerImage: CGFloat = 24
     static let listItemOuterImage: CGFloat = 40
+    static let headerImage: CGFloat = 80
 }
 
 final class Height {
     static let actionButton: CGFloat = 48
     static let estimatedTextInputField: CGFloat = 52
-    static let imageHeader: CGFloat = 240
     static let progressBar: CGFloat = 8
     static let segmentedControl: CGFloat = 34
     static let separator: CGFloat = 1
