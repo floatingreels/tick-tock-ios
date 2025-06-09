63957217db9a1a20abba7a11a9564a35aae1d3a3
David Gunzburg <build@paybonsai.com>
2025-06-09 15:50:42 +0200

    add new resources, remove unused file

--- TickTock/TickTock/Resources/Strings/Translation.swift
+++ TickTock/TickTock/Resources/Strings/Translation.swift
@@ -11,18 +11,30 @@ import Foundation
 enum Translation {}
 
 extension Translation {
+    
+    enum Error: String.LocalizationValue {
+        case general = "error_general_message"
+        
+        var val: String {
+            let res = LocalizedStringResource(self.rawValue, table: "Localizable")
+            return String(localized: res)
+        }
+    }
         
     enum General: String.LocalizationValue {
+        case buttonClose = "general_button_close"
+        case buttonCreate = "general_button_create"
         case buttonNext = "general_button_next"
+        case emailValidation = "general_email_validation_message"
         case labelEmail = "general_label_email"
-        case labelPassword = "general_label_password"
-        case labelPasswordConfirm = "general_label_password_confirm"
         case labelFirstName = "general_label_name_first"
         case labelLastName = "general_label_name_last"
-        case passwordRequirements = "general_password_requirements_message"
-        case emailValidation = "general_email_validation_message"
+        case labelPassword = "general_label_password"
+        case labelPasswordConfirm = "general_label_password_confirm"
         case passwordMismatch = "general_password_mismatch"
+        case passwordRequirements = "general_password_requirements_message"
         case successScreenMessage = "general_success_message"
+        case successNavTitle = "general_success_navigation_title"
         
         var val: String {
             let res = LocalizedStringResource(self.rawValue, table: "Localizable")
@@ -31,18 +43,18 @@ extension Translation {
     }
     
     enum Startup: String.LocalizationValue {
-        case homeNavTitle = "home_navigation_title"
-        case loginNavTitle = "login_navigation_title"
-        case registerNavTitle = "register_navigation_title"
-        case buttonLogin = "welcome_button_login"
-        case buttonRegister = "welcome_button_register"
         case buttonAddClient = "home_button_add_client"
         case buttonListClients = "home_button_list_clients"
         case buttonListProjects = "home_button_list_projects"
+        case buttonLogin = "welcome_button_login"
         case buttonNewProject = "home_button_new_project"
+        case buttonRegister = "welcome_button_register"
         case buttonStartSession = "home_button_start_session"
-        case registerFormDescription = "register_form_description"
+        case homeNavTitle = "home_navigation_title"
         case loginFormDescription = "login_form_description"
+        case loginNavTitle = "login_navigation_title"
+        case registerFormDescription = "register_form_description"
+        case registerNavTitle = "register_navigation_title"
         
         var val: String {
             let res = LocalizedStringResource(self.rawValue, table: "Localizable")
@@ -53,9 +65,29 @@ extension Translation {
     enum Client: String.LocalizationValue {
         case addClientNavTitle = "add_client_navigation_title"
         case addClientSuccessMessage = "add_client_success_message"
-        case clientNameLabel = "client_name_label"
+        case addClientFormDescription = "add_client_form_description"
+        case clientNameLabel = "client_label_name"
         case listClientsNavTitle = "list_clients_navigation_title"
         case detailClientNavTitle = "detail_client_navigation_title"
+        
+        var val: String {
+            let res = LocalizedStringResource(self.rawValue, table: "Localizable")
+            return String(localized: res)
+        }
+    }
+    
+    enum Project: String.LocalizationValue {
+        case addProjectNavTitle = "add_project_navigation_title"
+        case addProjectSuccessMessage = "add_project_success_message"
+        case addProjectFormDescription = "add_project_form_description"
+        case projectNameLabel = "project_label_name"
+        case listProjectsNavTitle = "list_project_navigation_title"
+        case detailProjectNavTitle = "detail_project_navigation_title"
+        case projectRateLabel = "project_label_rate"
+        case projectRateTypeHour = "project_rate_type_hour"
+        case projectRateTypeDay = "project_rate_type_day"
+        case projectRateTypeWeek = "project_rate_type_week"
+        case projectRateTypeMonth = "project_rate_type_month"
         
         var val: String {
             let res = LocalizedStringResource(self.rawValue, table: "Localizable")
