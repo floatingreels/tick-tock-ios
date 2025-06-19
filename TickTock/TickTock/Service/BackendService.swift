dd8aa4bd147e70ab786473eeaabe394bebdb3c8f
David Gunzburg <build@paybonsai.com>
2025-06-19 10:16:41 +0200

    move protocols to separate files

--- TickTock/TickTock/Service/BackendService.swift
+++ TickTock/TickTock/Service/BackendService.swift
@@ -4,26 +4,9 @@
 //
 //  Created by David Gunzburg on 21/05/2025.
 //
+
 import Foundation
 import Alamofire
-
-protocol Requestable {
-    var relativeURL: URL? { get }
-    var method: RequestMethod { get }
-    var query: RequestQuery? { get }
-    var body: RequestBody? { get }
-}
-
-public enum RequestMethod: String {
-    
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
-}
-
-typealias RequestQuery = [String: String]
-typealias RequestBody = [String: Any]
 
 final class BackendService {
     
